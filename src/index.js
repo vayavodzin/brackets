f719f2b153343348564dc31e7d32d3aca9afd8ef
v.vayavodzin <v.vayavodzin@godeltech.com>
2019-03-13 14:19:31 +0300

    feat: add correct brackets check;

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strlen = str.length;
+
+  for(let i = 0; i < strlen; i++) {
+    bracketsConfig.forEach(function (bracket) {
+      let bracketPair = bracket.join('');
+      let substr = str.indexOf(bracketPair);
+
+      if (substr !== -1) {
+        str = str.replace(bracketPair, '');
+      }
+    });
+  }
+
+  return !str;
 }
